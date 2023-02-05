e47075679707cae4f007c516eca03a0547f77a79
kalwi7 <dkalwasinski717@gmail.com>
2023-02-05 19:39:36 +0100

    css update, fetching opinions

--- components/Opinions.js
+++ components/Opinions.js
@@ -2,37 +2,11 @@ import classes from "./CommentForm.module.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const Opinions = ({id, opinions}) => {
-  // const [opinions, setOpinions] = useState();
+const Opinions = ({id, loadedOpinions}) => {
+  // const [opinions, setOpinions] = useState()
 
 
-  // useEffect(() => {
-  //   const loadedOpinions = [];
-
-  //   axios
-  //     .get(
-  //       "https://react-training-fffc3-default-rtdb.europe-west1.firebasedatabase.app/opinions.json"
-  //     )
-  //     .then(function (response) {
-  //       for (const key in response.data) {
-  //         loadedOpinions.push({
-  //           id: response.data[key].id,
-  //           name: response.data[key].name,
-  //           text: response.data[key].opinion,
-  //         });
-  //       }
-  //       setOpinions(loadedOpinions.filter( opinion => opinion.id === id));
-  //       // const currentProductOpinions = opinions.filter( opinion => opinion.id === id )
-  //       console.log(opinions)
-  //     })
-  //     .catch(function (error) {
-  //       // handle error
-  //       console.log(error);
-  //     })
-  //     .then(function () {
-  //       // always executed
-  //     });
-  // }, []);
+ 
 
   // useEffect(() => {
   //   setOpinionsContent(opinions?.map(opinion => {
@@ -59,44 +33,52 @@ const Opinions = ({id, opinions}) => {
   return (
     <section>
       <ul>Opinie</ul>
-      {console.log(opinions)}
+      {console.log(loadedOpinions)}
     </section>
   );
 };
 
 
-export async function getStaticProps() {
-  const loadedOpinions = [];
+export async function getServerSideProps() {
 
-  axios
-    .get(
-      "https://react-training-fffc3-default-rtdb.europe-west1.firebasedatabase.app/opinions.json"
-    )
-    .then(function (response) {
-      for (const key in response.data) {
-        loadedOpinions.push({
-          id: response.data[key].id,
-          name: response.data[key].name,
-          text: response.data[key].opinion,
-        });
-      }
-      // setOpinions(loadedOpinions.filter( opinion => opinion.id === id));
-      // const currentProductOpinions = opinions.filter( opinion => opinion.id === id )
-      // console.log(opinions)
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
+  // const allFragrances = await fetchApi(
+  //   `${baseUrl}/products/list?categoryId=cat160006&pageSize=200`
+  // );
 
-  return {
-    props: {
-      opinions: loadedOpinions.filter( opinion => opinion.id === id),
-    },
-  };
-}
+    // const loadedOpinions = [];
+
+    const loadedOpinions = await axios
+      .get(
+        "https://react-training-fffc3-default-rtdb.europe-west1.firebasedatabase.app/opinions.json"
+      )
+      .then(function (response) {
+
+        
+        // for (const key in response.data) {
+        //   loadedOpinions.push({
+        //     id: response.data[key].id,
+        //     name: response.data[key].name,
+        //     text: response.data[key].opinion,
+        //   });
+        // }
+        // setOpinions(loadedOpinions.filter( opinion => opinion.id === id));
+        // const currentProductOpinions = opinions.filter( opinion => opinion.id === id )
+        console.log(response)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+      
+      return {
+        props: {
+          loadedOpinions: loadedOpinions,
+        },
+      };
+      
+    };
 
 export default Opinions;
