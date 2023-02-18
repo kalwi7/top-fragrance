85b2e32ecf4a912c1ea7446579899541cca22086
kalwi7 <dkalwasinski717@gmail.com>
2023-02-18 14:36:59 +0100

    added progressBar on loading

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -2,17 +2,18 @@ import Router from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
+NProgress.configure({
+  minimum: 0.6,
+  easing: "ease",
+  speed: 1000,
+  showSpinner: false,
+});
 const ProgressBar = () => {
-  NProgress.configure({
-    minimum: 0.6,
-    easing: "ease",
-    speed: 1000,
-    showSpinner: false,
-  });
-
   Router.events.on("routeChangeStart", () => NProgress.start());
   Router.events.on("routeChangeComplete", () => NProgress.done());
   Router.events.on("routeChangeError", () => NProgress.done());
+
+  return null;
 };
 
 export default ProgressBar;
